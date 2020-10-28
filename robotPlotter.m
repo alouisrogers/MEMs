ceb0f50059fab0e4470e6e24db5bd009a98b2d59
Noah Ossanna <noahossa@gmail.com>
2020-10-27 21:13:28 -0400

    .

--- robotPlotter.m
+++ robotPlotter.m
@@ -1,47 +1,62 @@
+clc; clear;
 %Noah Ossanna 10/25/20
 %Script to plot robot trajectories for different geometries using:
 %basicRobot Class
 
 %define time of simulation
 dt = 0.1;
-t = 0:dt:5;
+t = 0:dt:15;
 
 %define properties of first robot class
-robot1 = basicRobot;
-%robot1.ID = 'robot1';
-robot1.phi       = 4;           %robot wheel velocities (shared)
-robot1.alpha1    = -pi/2;       %angle wheelbase 1
-robot1.alpha2    = pi/2;        %angle wheelbase 2
-robot1.beta1     = pi;          %angle wheel 1
-robot1.beta2     = 0;           %angle wheel 2
-robot1.r1        = 0.25;        %wheel 1 radius
-robot1.r2        = 0.1;         %wheel 2 radius
-robot1.b         =0.4;          %wheel base of robot
-robot1.theta0    = pi/2;        %initial heading of the robot
+              %(robotID, phi1, phi2, alpha1, alpha2, beta1, beta2, r1, r2, b, theta0)
+robot1 = basicRobot('robot1', 0.7, -0.7, pi/2, -pi/2, 0, pi, 0.1, 0.3, 0.5, pi/2);
 
-xPose = [];
-yPose = [];
-thetaPose = [];
 
-for i = 1:length(t)
+poseX = zeros(1,length(t));
+poseY = zeros(1,length(t));
+poseTheta = zeros(1,length(t));
+poseTheta(1) = robot1.theta0;
+
+for i = 2:length(t)+1
     globalVelocity = robot1.getVelocity(robot1);
     xdot = globalVelocity(1);
     ydot = globalVelocity(2);
     omega = globalVelocity(3);
     
-    %globalPose = robot1.getPose(dt, xdot, ydot, omega);
-    xPose(i) = xdot*dt; %globalPose(1);
-    yPose(i) = ydot*dt;%globalPose(2);
-    thetaPose(i) = omega*dt;%globalPose(3);
+    poseChange = robot1.getPose(dt, xdot, ydot, omega);     %returns dx, dy, dtheta (change in each var given over the given step)
+    poseX(i) = poseX(i-1) + poseChange(1);
+    poseY(i) = poseX(i-1) + poseChange(2);
+    poseThetaLocal = poseTheta(i-1) + poseChange(3);
     
-    robot1.theta0 = thetaPose(i); %update robot heading based on most recent timestep calc
+    poseTheta(i) = wrapTo2Pi(poseThetaLocal);
+    
+    robot1.theta0 = poseThetaLocal; %update robot heading based on most recent timestep calc
+    
 end
 
+poseTheta = wrapTo360(rad2deg(poseTheta));
 figure(1111)
-plot(xPose,yPose, 'r.')
 %title('Robot Position Over ', num2str(t(end)),' Seconds')
+title('Robot Position vs. Time')
+plot(t,poseY(1:end-1), 'r.')
+hold on
+plot(t,poseX(1:end-1), 'm.')
+xlabel('time (s)')
+ylabel('X and Y Position (m)')
+legend('t vs. x', 't vs. y')
+
+figure(1112)
+plot(poseX,poseY, 'b.')
+title('X Versus Y Position')
 xlabel('X Position (m)')
 ylabel('Y Position (m)')
+
+figure(1113)
+plot(t,poseTheta(1:end-1))
+title('Theta Versus Time')
+xlabel('time (s)')
+ylabel('Mapped Heading (0-360 Degrees)')
+
 
 
 
